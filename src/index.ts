cd0b1578f31ea04a7b682db1b9c62fbc1f25acd0
dannyel-zhelyazkov <64685665+dannyel-zhelyazkov@users.noreply.github.com>
2021-10-09 15:16:17 +0300

    Revert "Feature/endpoints favorites"

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { connectDb } from './database';
-import { favoriteRouter } from './routes';
 
 dotenv.config();
 
@@ -15,13 +13,7 @@ app.use(
 		origin: 'http://localhost:3000',
 	}),
 );
-app.use(express.urlencoded());
-app.use(express.json());
 
-app.use('/favorites', favoriteRouter);
-
-connectDb().then(() =>
-	app.listen(port, () => {
-		console.log(`Server is running on http://localhost:${port}`);
-	}),
-);
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
